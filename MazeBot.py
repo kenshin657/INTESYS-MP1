d6a8a3daed2286f8f4a4e7ad92302f76a309efa2
Kyle T <kyle_tagle@dlsu.edu.ph>
2019-10-21 17:54:27 +0800

    added gui TODO: PathFinding

--- MazeBot.py
+++ MazeBot.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 
 class Prompt:
     def __init__(self, master):
+        self.master = master
         frame = Frame(master)
         frame.pack()
 
@@ -22,16 +23,103 @@ class Prompt:
         if int(input) < 8 or int(input) > 64:
             messagebox.showerror('Invalid Input', message='Please enter a valid number')
         else:
-            print('STUFF')
+            self.master.destroy()
+            app = Tk()
 
-class Maze:
-    colorBG = "red"
-    emptyColor = "white"
-    colorBorder = "red"
-    emptyBorder = "black"
+            if int(input) < 25:
+                cellSize = 30
+            else:
+                cellSize = 15
 
-    def __init__(self, master, x, y):
-        frame = Frame(master)
+            grid = CellGrid(app, int(input), int(input), cellSize)
+            grid.pack()
+
+            app.mainloop()
+
+
+#https://stackoverflow.com/questions/30023763/how-to-make-an-interactive-2d-grid-in-a-window-in-python
+class Cell():
+    FILLED_COLOR_BG = "black"
+    EMPTY_COLOR_BG = "white"
+    FILLED_COLOR_BORDER = "black"
+    EMPTY_COLOR_BORDER = "black"
+
+    def __init__(self, master, x, y, size):
+        """ Constructor of the object called by Cell(...) """
+        self.master = master
+        self.abs = x
+        self.ord = y
+        self.size= size
+        self.fill= False
+
+    def _switch(self):
+        """ Switch if the cell is filled or not. """
+        self.fill= not self.fill
+
+    def draw(self):
+        """ order to the cell to draw its representation on the canvas """
+        if self.master != None :
+            fill = Cell.FILLED_COLOR_BG
+            outline = Cell.FILLED_COLOR_BORDER
+
+            if not self.fill:
+                fill = Cell.EMPTY_COLOR_BG
+                outline = Cell.EMPTY_COLOR_BORDER
+
+            xmin = self.abs * self.size
+            xmax = xmin + self.size
+            ymin = self.ord * self.size
+            ymax = ymin + self.size
+
+            self.master.create_rectangle(xmin, ymin, xmax, ymax, fill = fill, outline = outline)
+
+class CellGrid(Canvas):
+    def __init__(self,master, rowNumber, columnNumber, cellSize, *args, **kwargs):
+        Canvas.__init__(self, master, width = cellSize * columnNumber , height = cellSize * rowNumber, *args, **kwargs)
+
+        self.cellSize = cellSize
+
+        self.grid = []
+        for row in range(rowNumber):
+
+            line = []
+            for column in range(columnNumber):
+                line.append(Cell(self, column, row, cellSize))
+
+            self.grid.append(line)
+
+        #memorize the cells that have been modified to avoid many switching of state during mouse motion.
+        self.switched = []
+
+        #bind click action
+        self.bind("<Button-1>", self.handleMouseClick)
+        #bind moving while clicking
+        #self.bind("<B1-Motion>", self.handleMouseMotion)
+        #bind release button action - clear the memory of midified cells.
+        self.bind("<ButtonRelease-1>", lambda event: self.switched.clear())
+
+        self.draw()
+
+
+
+    def draw(self):
+        for row in self.grid:
+            for cell in row:
+                cell.draw()
+
+    def _eventCoords(self, event):
+        row = int(event.y / self.cellSize)
+        column = int(event.x / self.cellSize)
+        return row, column
+
+    def handleMouseClick(self, event):
+        row, column = self._eventCoords(event)
+        cell = self.grid[row][column]
+        cell._switch()
+        cell.draw()
+        #add the cell to the list of cell switched during the click
+        self.switched.append(cell)
+
 
 
 root = Tk()
